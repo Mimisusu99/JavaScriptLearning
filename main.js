6f33e3fc523336b36a7470209b3ed16d4a71b3ec
Anshori Suhendro <anshorishndr@gmail.com>
2025-06-17 23:26:21 +0700

    Data Type

--- main.js
+++ main.js
@@ -17,3 +17,35 @@ console.log(varVariable)
 // console.log(constVariable)
 // constVariable = "Hello from Const below" 
 // constVariable = "Hello from Const again"
+
+let undefinedValue
+console.log(undefinedValue)
+
+let nullValue = null
+console.log(null)
+
+let numberValue =10
+console.log(numberValue)
+
+let stringValue = "Hello, World"
+console.log(stringValue)
+
+let booleanValue = true
+console.log(booleanValue)
+booleanValue = false
+console.log(typeof booleanValue)
+
+let objectValue = {
+    name: "John",
+    age: 30,
+    city: "New York"
+}
+console.log(objectValue)
+
+let arrayValue = [1, 2, 3, 4, 5]
+console.log(arrayValue)
+
+let functionValue = function() {
+    return "Hello from Function";
+}
+console.log(functionValue());
