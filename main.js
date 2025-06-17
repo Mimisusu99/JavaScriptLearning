02ecdd2b8279bc03999bf121ede45c47439c7112
Anshori Suhendro <anshorishndr@gmail.com>
2025-06-17 23:16:25 +0700

    variable

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("Hello, World External");
+// console.log("Hello, World External");
+
+var varVariable = "Hello from Var"
+console.log(varVariable)
+// var varVariable = "Hello from Var below"
+// console.log(varVariable)
+// var varVariable = "Hello from Var again"
+// console.log(varVariable)
+
+// let letVariable = "Hello from Let"
+// console.log(letVariable)
+// let letVariable = "Hello from Let below"
+// let letVariable = "Hello from Let below"
+// console.log(letVariable)
+
+// const constVariable = "Hello from Const"
+// console.log(constVariable)
+// constVariable = "Hello from Const below" 
+// constVariable = "Hello from Const again"
