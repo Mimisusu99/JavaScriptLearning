44b08ba4027f22422ef478d4db88173915dd3589
Anshori Suhendro <anshorishndr@gmail.com>
2025-06-18 19:42:51 +0700

    arithmetic operation

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 // console.log("Hello, World External");
 
-var varVariable = "Hello from Var"
-console.log(varVariable)
+//==Variable Declaration==//
+
+// var varVariable = "Hello from Var"
+// console.log(varVariable)
 // var varVariable = "Hello from Var below"
 // console.log(varVariable)
 // var varVariable = "Hello from Var again"
@@ -18,34 +20,74 @@ console.log(varVariable)
 // constVariable = "Hello from Const below" 
 // constVariable = "Hello from Const again"
 
-let undefinedValue
-console.log(undefinedValue)
+//data type
 
-let nullValue = null
-console.log(null)
+// let undefinedValue
+// console.log(undefinedValue)
 
-let numberValue =10
-console.log(numberValue)
+// let nullValue = null
+// console.log(null)
 
-let stringValue = "Hello, World"
-console.log(stringValue)
+// let numberValue =10
+// console.log(numberValue)
 
-let booleanValue = true
-console.log(booleanValue)
-booleanValue = false
-console.log(typeof booleanValue)
+// let stringValue = "Hello, World"
+// console.log(stringValue)
 
-let objectValue = {
-    name: "John",
-    age: 30,
-    city: "New York"
-}
-console.log(objectValue)
+// let booleanValue = true
+// console.log(booleanValue)
+// booleanValue = false
+// console.log(typeof booleanValue)
 
-let arrayValue = [1, 2, 3, 4, 5]
-console.log(arrayValue)
+// let objectValue = {
+//     name: "John",
+//     age: 30,
+//     city: "New York"
+// }
+// console.log(objectValue)
 
-let functionValue = function() {
-    return "Hello from Function";
-}
-console.log(functionValue());
+// let arrayValue = [1, 2, 3, 4, 5]
+// console.log(arrayValue)
+
+// let functionValue = function() {
+//     return "Hello from Function";
+// }
+// console.log(functionValue());
+
+// Arithmetic Operators
+
+let num1 = 2, num2 = 3, num3 = 5
+
+// +
+console.log(10 + 5)
+console.log(num1 + num2)
+
+// -
+console.log(10 - 5)
+console.log(num2 -num1)
+
+// *
+console.log(2 * 10)
+console.log(num2 * num3)
+
+// /
+console.log(10 / 2)
+console.log(num3 / num2)
+
+// **
+console.log(2 ** 3) 
+console.log(num2 ** num1)
+
+// %
+console.log(6%2)
+console.log(num3 % num2)
+
+// ++
+console.log(num1++) // Postfix increment
+console.log(num1) // Pre-increment
+console.log(++num1) // Post-decrement
+
+// --
+console.log(num2--) // Postfix decrement
+console.log(num2) // Pre-decrement
+console.log(--num2) // Post-decrement
