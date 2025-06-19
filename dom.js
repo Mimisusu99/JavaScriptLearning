1b495a3149f8a80d2a316652b327b1c64501568a
Anshori Suhendro <anshorishndr@gmail.com>
2025-06-19 23:04:05 +0700

    DOM Manipulation

---
+++ dom.js
@@ -0,0 +1,8 @@
+// let el = document.getElementById("div-1")
+// let el = document.getElementsByTagName("div")
+// let el = document.getElementsByClassName("div-2")
+
+let el = document.querySelector("#div-1")
+let el = document.querySelector("div")
+let el = document.querySelector(".div-2")
+console.log(el)
