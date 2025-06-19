95b5c781d224391ee68fdf76daaac9c1cfa8348a
Anshori Suhendro <anshorishndr@gmail.com>
2025-06-19 23:16:06 +0700

    DOM Manipulation editing HTML

--- dom.js
+++ dom.js
@@ -1,8 +1,26 @@
+// DOM Manipulation Search
 // let el = document.getElementById("div-1")
 // let el = document.getElementsByTagName("div")
 // let el = document.getElementsByClassName("div-2")
 
-let el = document.querySelector("#div-1")
-let el = document.querySelector("div")
-let el = document.querySelector(".div-2")
-console.log(el)
+// QUERY SELECTOR
+// let el = document.querySelector("#div-1")
+// let el = document.querySelector("div")
+// let el = document.querySelector(".div-2")
+// console.log(el)
+
+// DOM MANIPULATION HTML ELEMENTS CHANGING
+
+// let el = document.querySelector("#div-1")
+// console.log(el)
+
+// el.innerHTML = "<p>Hello World</p>"
+
+// let el = document.getElementById("input")
+// console.log(input)
+
+// input.setAttribute("type", "checkbox")
+
+// let el = document.getElementById("input")
+// Input.style.borderColor = "red"
+// input.style.color = "blue"
