0f887976bf4f2490eb5a13faa507c62793034958
Anshori Suhendro <anshorishndr@gmail.com>
2025-06-19 23:49:23 +0700

    DOM Event

--- dom.js
+++ dom.js
@@ -27,10 +27,28 @@
 
 // ADDING AND DELETING ELEMENTS
 
-const pElement = document.createElement("p")
-const divEl = document.getElementById("div-1")
+// const pElement = document.createElement("p")
+// const divEl = document.getElementById("div-1")
 
-divEl.appendChild(pElement)
-pElement.innerHTML = "P, Punteun"
+// divEl.appendChild(pElement)
+// pElement.innerHTML = "P, Punteun"
 
-divEl.removeChild(pElement)
+// divEl.removeChild(pElement)
+
+// const hello = document.getElementById("hello")
+
+// hello.addEventListener("mouseenter", function() {
+//     // console.log("Mouse enter hello world text")
+//     hello.style.border = "1px solid blue"
+// })
+
+// const btn = document.getElementById("btn")
+// btn.addEventListener("click", function() {
+//     const boom = document.getElementById("boom")
+//     boom.style.display = "block"    
+// })
+
+function showBoom () {
+    const boom = document.getElementById("boom")
+    boom.style.display = "block"
+}
